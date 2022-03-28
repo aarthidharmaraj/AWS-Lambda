63ec2e4f93c57abe16d7a282cee3c3c0ad07c7b5
Aarthi Dharmaraj <aarthi.dharmaraj@aspiresys.com>
2022-03-28 17:30:34 +0530

    Python-Lambda function to list the buckets in s3

---
+++ acess_database.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+
+dynamodb=boto3.resource('dynamodb')
+## to get an item from database
+table=dynamodb.Table('Planets') ##database and table created in lambda database
+def lambda_handler(event,context):
+    response=table.get_item(
+        key={
+            'id':'mercury'
+        }
+    )
+    print(response)
+    return {
+        'statusCode':200,
+        'body': response
+    }
+    
+## to put an item to a database
+table=dynamodb.Table('Planets') ##database and table created in lambda database
+def lambda_handler(event,context):
+    table.put_item(
+        Item={
+            'id':'mercurynew',
+            'temp':'cold'
+        }
+    )
+    return {
+        'statusCode':200,
+        'body': 'Item added'
+    }
